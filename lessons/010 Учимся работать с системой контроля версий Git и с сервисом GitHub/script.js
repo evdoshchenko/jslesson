650582d14c12d9d4818cc00ba755ae3345936594
Danil <aka4j7@gmail.com>
2021-07-04 23:27:45 +0300

    second

--- lessons/010 Учимся работать с системой контроля версий Git и с сервисом GitHub/script.js
+++ lessons/010 Учимся работать с системой контроля версий Git и с сервисом GitHub/script.js
@@ -2,3 +2,10 @@
 // git config --global user.name "Danil"
 // git config --global user.email aka4j7@gmail.com 
 // git status
+// git add -A - добавить все файлы в индекс
+// git status
+// git add main.css - добавить конкрентный файл
+// git add *.css -
+// git status
+// git commit -a -m"first commit" - отправить в комит всего с месседжем
+
